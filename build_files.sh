24f73949cc8167c407bdc8ebd7aeeaa1c475f5fe
Pratik <pratik@example.com>
2026-03-06 17:53:17 +0530

    fix: Update build_files.sh to bypass externally-managed-environment with python3.9

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,8 @@
 # Build script for Vercel
 echo "Building the project..."
 # Install requirements
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt --break-system-packages
 
 # Collect static files
-python manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 echo "Build complete."
