4c2d7e5434e8ef5328e0ccaacbeccdc79f46f362
Pratik <pratik@example.com>
2026-03-06 17:56:11 +0530

    fix: Update build_files.sh to use default python with --break-system-packages flag

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,8 @@
 # Build script for Vercel
 echo "Building the project..."
 # Install requirements
-python3.9 -m pip install -r requirements.txt --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
 # Collect static files
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 echo "Build complete."
